0473b5b24818663d3465a1e1bf9785a07f043283
Gabriel Bechara <gabriel_bechara@yahoo.com>
2017-08-28 17:53:39 +0200

    Spring Boot and Mongo Example

--- src/main/java/osedevops/group/springbootmongo/repository/CarMongoRepository.java
+++ src/main/java/osedevops/group/springbootmongo/repository/CarMongoRepository.java
@@ -1,10 +1,8 @@
 package osedevops.group.springbootmongo.repository;
 
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.data.repository.NoRepositoryBean;
 
 import osedevops.group.springbootmongo.model.Car;
 
-@NoRepositoryBean
 
 public interface CarMongoRepository extends CrudRepository{}
