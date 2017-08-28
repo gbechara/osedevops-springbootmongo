85b6b78b2c18ffbca2dd509892dc7eeb7ebca7d8
Gabriel Bechara <gabriel_bechara@yahoo.com>
2017-08-28 17:27:07 +0200

    Spring Boot and Mongo Example

--- src/main/java/osedevops/group/springbootmongo/controllers/CarController.java
+++ src/main/java/osedevops/group/springbootmongo/controllers/CarController.java
@@ -10,28 +10,34 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 import osedevops.group.springbootmongo.model.Car;
 import osedevops.group.springbootmongo.repository.CarMongoRepository;
+import osedevops.group.springbootmongo.repository.CarSearchRepository;
 
 @Controller
 public class CarController {
-    
+
     @Autowired
     CarMongoRepository carRepository;
-    
+
+    @Autowired
+    CarSearchRepository carSearchRepository;
+
     @RequestMapping("/home")
     public String home(Model model) {
         model.addAttribute("carList", carRepository.findAll());
         return "home";
     }
-    
+
     @RequestMapping(value = "/addCar", method = RequestMethod.POST)
     public String addCar(@ModelAttribute Car car) {
         carRepository.save(car);
         return "redirect:home";
     }
-    
+
     @RequestMapping(value = "/search")
-    public String search(@RequestParam String search) {
+    public String search(Model model, @RequestParam String search) {
+        model.addAttribute("carList", carSearchRepository.searchCars(search));
+        model.addAttribute("search", search);
         return "home";
     }
-    
+
 }
