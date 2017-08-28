fdfed3d82d2c6c46b7b9565b0d41140134d9617d
Gabriel Bechara <gabriel_bechara@yahoo.com>
2017-08-28 18:09:33 +0200

    Spring Boot and Mongo Example

--- src/test/java/osedevops/group/springbootmongo/SpringbootmongoApplicationTests.java
+++ src/test/java/osedevops/group/springbootmongo/SpringbootmongoApplicationTests.java
@@ -2,15 +2,20 @@ package osedevops.group.springbootmongo;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+import osedevops.group.springbootmongo.SpringbootmongoApplication;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = SpringbootmongoApplication.class)
+@WebAppConfiguration
 public class SpringbootmongoApplicationTests {
-
-	@Test
-	public void contextLoads() {
-	}
-
+    
+    @Test
+    public void contextLoads() {
+    }
+    
 }
