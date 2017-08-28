a326b9d1687225eafc88a6a18b73919f911ed4d7
Gabriel Bechara <gabriel_bechara@yahoo.com>
2017-08-28 13:33:50 +0200

    Spring Boot and Mongo Example

--- src/main/java/osedevops/group/springbootmongo/controllers/CarController.java
+++ src/main/java/osedevops/group/springbootmongo/controllers/CarController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import .model.Car;
+import osedevops.group.springbootmongo.model.Car;
 import osedevops.group.springbootmongo.repository.CarMongoRepository;
 
 @Controller
