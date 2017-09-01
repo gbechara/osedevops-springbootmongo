6bd4eaf3ded166ced264f7997afb266f8449d679
Gabriel Bechara <gabriel_bechara@yahoo.com>
2017-09-01 14:20:16 +0200

    Spring Boot and Mongo Example

--- src/main/java/osedevops/group/springbootmongo/SpringbootmongoApplication.java
+++ src/main/java/osedevops/group/springbootmongo/SpringbootmongoApplication.java
@@ -4,8 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+
 @SpringBootApplication
 @EnableMongoRepositories("osedevops.group.springbootmongo.repository")
+@EnableCircuitBreaker
 public class SpringbootmongoApplication {
 
 	public static void main(String[] args) {
