858deecba5ff8c0393340fe94c987a1425238f9a
Gabriel Bechara <gabriel_bechara@yahoo.com>
2017-09-02 10:31:27 +0200

    Spring Boot and Mongo Example

--- src/main/java/osedevops/group/springbootmongo/SpringbootmongoApplication.java
+++ src/main/java/osedevops/group/springbootmongo/SpringbootmongoApplication.java
@@ -6,11 +6,46 @@ import org.springframework.data.mongodb.repository.config.EnableMongoRepositorie
 
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 
+import com.uber.jaeger.Configuration;
+import com.uber.jaeger.samplers.ProbabilisticSampler;
+
+import brave.Tracing;
+import brave.opentracing.BraveTracer;
+import brave.sampler.Sampler;
+import zipkin.Span;
+import zipkin.reporter.AsyncReporter;
+import zipkin.reporter.Encoding;
+import zipkin.reporter.okhttp3.OkHttpSender;
+
 @SpringBootApplication
 @EnableMongoRepositories("osedevops.group.springbootmongo.repository")
 @EnableCircuitBreaker
 public class SpringbootmongoApplication {
 
+    @Bean
+	public io.opentracing.Tracer jaegerTracer() {
+		return new Configuration("spring-boot", new Configuration.SamplerConfiguration(ProbabilisticSampler.TYPE, 1),
+				new Configuration.ReporterConfiguration())
+				.getTracer();
+	}
+
+	//@Bean
+	public io.opentracing.Tracer zipkinTracer() {
+		OkHttpSender okHttpSender = OkHttpSender.builder()
+				.encoding(Encoding.JSON)
+				.endpoint("http://localhost:9411/api/v1/spans")
+				.build();
+		AsyncReporter<Span> reporter = AsyncReporter.builder(okHttpSender).build();
+		Tracing braveTracer = Tracing.newBuilder()
+				.localServiceName("spring-boot")
+				.reporter(reporter)
+				.traceId128Bit(true)
+				.sampler(Sampler.ALWAYS_SAMPLE)
+				.build();
+		return BraveTracer.create(braveTracer);
+	}
+
+    
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootmongoApplication.class, args);
 	}
